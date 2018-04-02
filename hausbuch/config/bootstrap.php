52864db680da83011e21d9e11119f6897970a321
vagrant <vagrant@localhost.localdomain>
2018-04-02 16:44:00 +0000

    initial commit

--- hausbuch/config/bootstrap.php
+++ hausbuch/config/bootstrap.php
@@ -78,7 +78,9 @@ try {
  * shared configuration.
  */
 //Configure::load('app_local', 'default');
-
+if (isset($_ENV['CAKE_ENV'])) {
+    Configure::load('app_' . $_ENV['CAKE_ENV'], 'default');
+}
 /*
  * When debug = true the metadata cache should only last
  * for a short time.
